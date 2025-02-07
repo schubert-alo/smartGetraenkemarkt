5804e53d8690c0b9c497053f8cb2cb8a4454985e
“simon <simon.schubert@aloberlin.de>
2025-02-07 20:08:59 +0100

    warnings fixed REST-API (just GET) is running

---
+++ src/main/java/org/salesAPI/controller/Hello.java
@@ -0,0 +1,13 @@
+package org.salesAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hallo, IT-KLasse!";
+    }
+}
