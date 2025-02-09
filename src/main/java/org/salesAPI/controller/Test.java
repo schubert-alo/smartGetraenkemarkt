4a461be16b6a2b11d8e3297d0f9795a6075b813f
“simon <simon.schubert@aloberlin.de>
2025-02-09 14:02:13 +0100

    Test-Klasse umbenannt. Das Projekt kann getestet werden mit dem Aufruf http://localhost:8080/hello im Browser

--- src/main/java/org/salesAPI/controller/Test.java
+++ src/main/java/org/salesAPI/controller/Test.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Hello {
+public class Test {
 
     @GetMapping("/hello")
     public String sayHello() {
