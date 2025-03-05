f3eaa7ee47ea049def6b15a8935feb40807e3e0a
Schubert <simon.schubert@aloberlin.de>
2025-03-05 09:02:33 +0100

    rename: Test.java --> TestController.java

--- src/main/java/org/salesAPI/controller/TestController.java
+++ src/main/java/org/salesAPI/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 // TestKlasse, um zu testen, dass die RESTfull API läuft
 // Eingabe im Browser: http://localhost:8080/hello
 @RestController
-public class Test {
+public class TestController {
 
     @GetMapping("/hello")
     public String sayHello() {
